1c3fbaf3791a1018f12582f131f3e7f3f2492ae8
wilson <wilsonmv90@gmail.com>
2023-06-08 23:08:23 -0500

    simulando una api

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,14 +1,24 @@
-export default function Article(props) {
+function Article({ products }) {
   return (
     <>
       <article className="article__item">
-        <h2 className="article__title">{props.article__title}</h2>
-        <img className="article__img" src={props.article__img} alt={props.article__title} />
-        <p className="article__paragrah">{props.article__paragrah}</p>
+        {products.map((product) => (
+          <div key={product.id}>
+            <h2 className="article__title">{product.name}</h2>
+            <img
+              className="article__img"
+              src={product.image}
+              alt={product.name}
+            />
+            <p className="article__paragrah">{product.texto}</p>
+          </div>
+        ))}
       </article>
     </>
   );
-} 
+}
+
+export default Article; 
 
 
 
